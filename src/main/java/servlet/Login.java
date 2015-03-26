2146416a4d9a263b6fde6263348fd21d98f052d8
klarhant <kevin.larhant@capgemini.com>
2015-03-26 17:52:01 +0100

    ajout servlet login

---
+++ src/main/java/servlet/Login.java
@@ -0,0 +1,8 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "Login", urlPatterns = "/login")
+public class Login  extends HttpServlet {
+}
