adfb3c58a8b01629f9cd5eb80d02415a0b0fecb8
klarhant <kevin.larhant@capgemini.com>
2015-04-10 14:49:52 +0200

    Ajout lib css / js

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -1,9 +1,45 @@
 package servlet;
 
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "Login", urlPatterns = "/login")
-public class Login  extends HttpServlet {
+public class Login extends HttpServlet {
 
+    static Logger log = Logger.getLogger(Login.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher;
+
+        if (request.getSession().getAttribute("login") != null) {
+            response.sendRedirect("index.jsp");
+        } else {
+            requestDispatcher = request.getRequestDispatcher("/login.jsp");
+            requestDispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pwd = request.getParameter("pwd");
+
+//        if (DaoUser.checkCredentials(login, pwd)) {
+//            request.getSession().setAttribute("login", login);
+//        }
+
+        response.sendRedirect("index.jsp");
+    }
 }
